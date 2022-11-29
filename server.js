98169ae764eb028f416e588f80a025f39c00819b
Homero Gelves <homerogelves@gmail.com>
2022-11-29 19:19:40 -0300

    29 11

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.delete('/products/:id', (req, res)=>{
     }
     //con el metodo filter lo elimino.
 
-    products = products.filter(p => p.id !== parseInt(req.params.id)) //Me saca el id que ingrese yo.
+    products = products.filter(p => p.id !== parseInt(req.params.id)) //Me saca el id que ingrese yo
 
     res.sendStatus(204)
 })
